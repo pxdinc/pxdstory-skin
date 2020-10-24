ef41fb68da4152599422af35ce6eef5746cdf3e3
gunggmee <gunggmee@gmail.com>
2020-10-24 23:42:38 +0900

    fix: minor tweaks

--- src/observable.js
+++ src/observable.js
@@ -1,33 +1,35 @@
 window.addEventListener("DOMContentLoaded", async () => {
-  // find P elements and convert into DIVs
-  const ps = [...document.querySelectorAll("p")].filter(p => p.textContent.trim().startsWith("ob:"))
-  ps.forEach(p => {
-    const parent = p.parentNode
-    const div = document.createElement('div')
-    div.dataset.ob = p.textContent.trim().split(":")[1]
-    parent.insertBefore(div, p)
-    parent.removeChild(p)
-  })
+  // find candidate elements, and convert into target elements
+  const candidateTags = new Set(['em', 'strong', 'i', 'b', 'span', 'q', 'p', 'div', 'blockquote', 'li'])
+  const candidates = [...document.querySelectorAll([...candidateTags].join(','))]
+  candidates
+    .filter(e => e.textContent.trim().startsWith("ob:"))
+    .forEach(e => {
+      const parent = e.parentNode
+      const wrapper = document.createElement(e.tagName)
+      wrapper.dataset.ob = e.textContent.trim().split(":")[1]
+      parent.insertBefore(wrapper, e)
+      parent.removeChild(e)
+    })
 
-  // find elements to embed cells
-  const cells = [...document.querySelectorAll("[data-ob]")].map((element) => {
-    const data = element.dataset.ob
-    const [, notebookKey, cellName] = data.match(/^(.+\/.+)\/(.+)$/)
+  // find target elements
+  const cells = [...document.querySelectorAll("[data-ob]")].map((e) => {
+    const [, notebookKey, cellName] = e.dataset.ob.match(/^(.+\/.+)\/(.+)$/)
     return {
-      element,
+      element: e,
       notebookKey,
       cellName,
     }
   })
-
   if (!cells.length) return
+  const cellMap = Object.fromEntries(cells.map(c => [`${c.notebookKey}/${c.cellName}`, c]))
 
   // load notebooks
-  const {Runtime, Inspector} = await import("https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js")
   const notebookKeys = [...new Set(cells.map((c) => c.notebookKey))]
-  const notebookModules = await Promise.all(
-    notebookKeys.map(key => import(`https://api.observablehq.com/@${key}.js?v=3`))
-  )
+  const [{Runtime, Inspector}, notebookModules] = await Promise.all([
+    import("https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js"),
+    Promise.all(notebookKeys.map(key => import(`https://api.observablehq.com/@${key}.js?v=3`))),
+  ])
   const notebooks = notebookModules.map((obj, i) => ({
     key: notebookKeys[i],
     module: obj.default,
@@ -39,8 +41,10 @@ window.addEventListener("DOMContentLoaded", async () => {
   document.body.appendChild(backstage)
   notebooks.forEach((nb) => {
     new Runtime().module(nb.module, (cellName) => {
-      const wrapper = document.createElement('div')
-      wrapper.dataset.ob = `${nb.key}/${cellName}`
+      const key = `${nb.key}/${cellName}`
+      const wrapperTag = cellMap[key] ? cellMap[key].element.tagName : 'div'
+      const wrapper = document.createElement(wrapperTag)
+      wrapper.dataset.ob = key
       backstage.appendChild(wrapper)
       return new Inspector(wrapper)
     })
@@ -53,7 +57,7 @@ window.addEventListener("DOMContentLoaded", async () => {
       if (!cell) return
 
       const element = backstage.querySelector(`[data-ob="${nb.key}/${cellName}"]`)
-      console.log(element)
+      element.style.overflowX = "auto";
       cell.element.parentNode.insertBefore(element, cell.element)
       cell.element.parentNode.removeChild(cell.element)
     })
